5fbf637c93ec1913a9d1db4fbf0606e11f471191
Afroanton <anton_zam@live.se>
2021-04-26 16:24:34 +0200

    done

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ void print_file(std::string filename)
 	std::ifstream file;
 	file.open(filename);
 	std::string content;
-	std::cout << "test" << "\n" << "N      T(s)      S" << "\n";
+	std::cout << filename << "\n" << "N      T(s)      S" << "\n";
 	while (!file.eof())
 	{
 		std::getline(file, content);
@@ -25,13 +25,13 @@ int main()
 {
 	
 	
+
 	auto random = [](int N) {std::vector<int>* data = random_data(N); return data;};
 	auto rising = [](int N) {std::vector<int>* data = rising_data(N); return data; };
 	auto decreasing = [](int N) {std::vector<int>* data = decreasing_data(N); return data; };
 	auto constant = [](int N) {std::vector<int>* data = constant_data(N); return data; };
 
-	int start = 1000;
-	int end = 10000;
+
 
 	auto sort_funk1 = [](std::vector<int>* random) {insertion_sort(random); };
 	auto sort_funk2 = [](std::vector<int>* random) {selection_sort(random); };
@@ -40,19 +40,21 @@ int main()
 	auto sort_funk5 = [](std::vector<int>* random) {std::sort(random->begin(), random->end()); };
 	auto sort_funk6 = [](std::vector<int>* random) {quicksort(&random->front(), &random->back()+1); };
 
-	
-	auto t = time(sort_funk3, decreasing(10000));
-	auto t2 = time(sort_funk6, decreasing(10000));
+	auto t = time(sort_funk3, rising(10000));
+	auto t2 = time(sort_funk6, rising(10000));
 
 	std::cout << t << std::endl;
 	std::cout << t2 << std::endl;
+
+	int start = 1000;
+	int end = 10000;
 	
 	remove("insertion_sort.data");
 	remove("selection_sort.data");
 	remove("quicksort.data");
 	remove("quicksort_mot.data");
 	remove("std_sort.data");
-	time_all(random, start,end);
+	time_all(rising, start,end);
 	print_file("insertion_sort.data");
 	print_file("selection_sort.data");
 	print_file("quicksort.data");
--- time.h
+++ time.h
@@ -10,14 +10,5 @@ void time_all(std::vector<int>*(*generate_data_funk)(int size), int start, int e
 
 void time_calculation(void(*sort_funk)(std::vector<int>*), std::vector<int>* container, std::string save_data);
 
-//void time_selection_sort(std::vector<int>* container);
-
-//void time_quicksort(std::vector<int>* container);
-
-//void time_quicksort_mot(std::vector<int>* container);
-
-//void time_std_sort(std::vector<int>* container);
-
 float time(void(*sort)(std::vector<int>*), std::vector<int>* container);
 
-//void time_intervall(void(*sort)(std::vector<int>*), std::vector<int>* (*generate_data_funk)(int size),int start,int end,std::string save_data);
